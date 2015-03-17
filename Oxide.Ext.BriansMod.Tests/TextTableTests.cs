410815181457364bedd46a7ac649fa2052694225
W. Brian Gourlie <bgourlie@gmail.com>
2015-03-17 12:03:20 -0500

    Add table formatter.

---
+++ Oxide.Ext.BriansMod.Tests/TextTableTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Oxide.Ext.BriansMod.Tests
+{
+	using Xunit;
+
+	public class TextTableTests
+	{
+		[Fact]
+		public void text_table_smoke_test()
+		{
+			var textTable = new TextTable("Weapon", "Num Kills", "Best Distance (meters)", "Distance Record Holder");
+			textTable.AddRow("AR", 5, 6.32431, "bgzee");
+			textTable.AddRow("Bow", 15, 50.2343, "brady");
+			textTable.AddRow("Bolt", 4, 90.2343, "tbs");
+			var output = textTable.ToString();
+		}
+	}
+}
---
+++ Oxide.Ext.BriansMod/TextTable.cs
@@ -0,0 +1,63 @@
+﻿namespace Oxide.Ext.BriansMod
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	public class TextTable
+	{
+		private readonly string[] _columns;
+		private readonly List<List<string>> _rows = new List<List<string>>();
+
+		public TextTable(params string[] columns)
+		{
+			_columns = columns;
+		}
+
+		public void AddRow(params object[] values)
+		{
+			if (values.Length > _columns.Length)
+			{
+				throw new Exception("Row has too many columns.");
+			}
+
+			_rows.Add(new List<string>(values.Select(v => v.ToString())));
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			var colWidths = _columns.Select(c => c.Length).ToArray();
+			for(var i = 0; i < _rows.Count; i++)
+			{
+				for (var j = 0; j < _rows[i].Count; j++)
+				{
+					if (_rows[i][j].Length > colWidths[j])
+					{
+						colWidths[j] = _rows[i][j].Length;
+					}
+				}
+			}
+
+			var totalWidth = colWidths.Aggregate((total, width) => total + width);
+			for(var i = 0; i < _columns.Length; i++)
+			{
+				sb.AppendFormat("| {0}{1} ", _columns[i], new string(' ', colWidths[i] - _columns[i].Length));
+			}
+			sb.AppendLine();
+			sb.AppendLine(new string('-', totalWidth + (_columns.Length * 3)));
+
+			foreach (var row in _rows)
+			{
+				for (var i = 0; i < row.Count; i++)
+				{
+					sb.AppendFormat("| {0}{1} ", row[i], new string(' ', colWidths[i] - row[i].Length));
+				}
+				sb.AppendLine();
+			}
+
+			return sb.ToString();
+		}
+	}
+}
